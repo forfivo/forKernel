87c4416c9403f2a3ebc7a33e8ca7c062d3dd3972
Steve Muckle <smuckle@codeaurora.org>
2014-07-22 15:12:32 -0700

    sched: fixes for compilation without CONFIG_SCHED_HMP
    These fixes are necessary to compile without CONFIG_SCHED_HMP
    enabled.
    
    Change-Id: Iabbde3c22a81288242ed3a44fdfdb2a16db8b072
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8309,11 +8309,13 @@ void __init sched_init(void)
 	sec_gaf_supply_rqinfo(offsetof(struct rq, curr),
 			      offsetof(struct cfs_rq, rq));
 
+#ifdef CONFIG_SCHED_HMP
 	if (num_possible_cpus() > 4)
 		sched_enable_hmp = sched_enable_power_aware = 1;
 
 	if (sched_enable_hmp)
 		pr_info("HMP scheduling enabled.\n");
+#endif
 
 	BUG_ON(num_possible_cpus() > BITS_PER_LONG);
 
