ca7840e7077b7d3d22579fb716a6a191d58f4986
forfivo <forfivo@gmail.com>
2015-03-20 17:58:48 -0300

    Allow logcat termination on suspend. and to have bypass for debugs.

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -26,6 +26,7 @@
 #include <linux/slab.h>
 #include <linux/time.h>
 #include "logger.h"
+#include <linux/earlysuspend.h>
 
 #include <asm/ioctls.h>
 #include <mach/sec_debug.h>
@@ -34,6 +35,12 @@
 #ifndef CONFIG_LOGCAT_SIZE
 #define CONFIG_LOGCAT_SIZE 256
 #endif
+
+static unsigned int log_enabled = 1;
+static unsigned int log_always_on = 0;
+
+module_param(log_always_on, uint, S_IWUSR | S_IRUGO);
+
 
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
@@ -466,6 +473,23 @@ static ssize_t do_write_log_from_user(struct logger_log *log,
 	return count;
 }
 
+static void log_early_suspend(struct early_suspend *handler)
+{
+	if (log_enabled)
+		log_enabled = 0;
+}
+
+static void log_late_resume(struct early_suspend *handler)
+{
+	if (!log_enabled)
+		log_enabled = 1;
+}
+
+static struct early_suspend log_suspend = {
+	.suspend = log_early_suspend,
+	.resume = log_late_resume,
+};
+
 /*
  * logger_aio_write - our write method, implementing support for write(),
  * writev(), and aio_write(). Writes are our fast path, and we try to optimize
@@ -479,6 +503,9 @@ ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 	ssize_t ret = 0;
+
+	if (!log_enabled && !log_always_on)
+		return 0;
 
 	now = current_kernel_time();
 
@@ -827,6 +854,8 @@ int sec_debug_subsys_set_logger_info(
 static int __init logger_init(void)
 {
 	int ret;
+
+	register_early_suspend(&log_suspend);
 
 	ret = init_log(&log_main);
 	if (unlikely(ret))
