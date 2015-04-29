25f2e144739db50f01b95f982a00f3a2d62eb402
forfivo <forfivo@gmail.com>
2015-04-29 01:21:22 -0300

    kernel/workqueue.c: avoid array subscript above array boundaries
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3740,7 +3740,11 @@ void freeze_workqueues_begin(void)
 		}
 
 		list_for_each_entry(wq, &workqueues, list) {
-			struct cpu_workqueue_struct *cwq = get_cwq(cpu, wq);
+			struct cpu_workqueue_struct *cwq;
+			if (cpu < CONFIG_NR_CPUS)
+				cwq = get_cwq(cpu, wq);
+			else
+				continue;
 
 			if (cwq && wq->flags & WQ_FREEZABLE)
 				cwq->max_active = 0;
@@ -3783,7 +3787,11 @@ bool freeze_workqueues_busy(void)
 		 * to peek without lock.
 		 */
 		list_for_each_entry(wq, &workqueues, list) {
-			struct cpu_workqueue_struct *cwq = get_cwq(cpu, wq);
+			struct cpu_workqueue_struct *cwq;
+			if (cpu < CONFIG_NR_CPUS)
+                                cwq = get_cwq(cpu, wq);
+                        else
+                                continue;
 
 			if (!cwq || !(wq->flags & WQ_FREEZABLE))
 				continue;
