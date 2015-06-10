023baadbda5cae9e66dae9ec67f4a1fdf9497ed9
forfivo <forfivo@gmail.com>
2015-06-10 11:40:26 -0300

    Fix for "sched: window-based load stats for tasks"

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3508,6 +3508,7 @@ static inline void schedule_debug(struct task_struct *prev)
 
 static void put_prev_task(struct rq *rq, struct task_struct *prev)
 {
+	update_task_ravg(prev, rq, 1);
 	if (prev->on_rq || rq->skip_clock_update < 0)
 		update_rq_clock(rq);
 	prev->sched_class->put_prev_task(rq, prev);
