6f3a65b5e4dba9d072105cf65ceb9257945ca9de
forfivo <forfivo@gmail.com>
2015-06-15 18:34:58 -0300

    cpufreq: Export user_policy min/max
    Add sysfs nodes for user_policy min and max settings.
    
    Bug 888312
    
    Change-Id: Ife8ec07f8faebc0c04e7ecf6e24976a311bad73b
    Reviewed-on: http://git-master/r/72201
    Signed-off-by: Antti P Miettinen <amiettinen@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/75882
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -483,6 +483,8 @@ show_one(cpuinfo_transition_latency, cpuinfo.transition_latency);
 show_one(scaling_min_freq, min);
 show_one(scaling_max_freq, max);
 show_one(cpu_utilization, util);
+show_one(policy_min_freq, user_policy.min);
+show_one(policy_max_freq, user_policy.max);
 
 static ssize_t show_scaling_cur_freq(
 	struct cpufreq_policy *policy, char *buf)
@@ -843,6 +845,8 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+cpufreq_freq_attr_ro(policy_min_freq);
+cpufreq_freq_attr_ro(policy_max_freq);
 cpufreq_freq_attr_rw(UV_mV_table);
 cpufreq_freq_attr_rw(UV_uV_table);
 cpufreq_freq_attr_rw(scaling_min_freq_gpu);
@@ -867,6 +871,8 @@ static struct attribute *default_attrs[] = {
 	&scaling_available_governors.attr,
 	&cpufreq_freq_attr_scaling_available_freqs.attr,
 	&scaling_setspeed.attr,
+	&policy_min_freq.attr,
+	&policy_max_freq.attr,
 	&UV_mV_table.attr,
 	&UV_uV_table.attr,
 	&scaling_min_freq_gpu.attr,
