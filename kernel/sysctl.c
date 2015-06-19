cc6a7875e50946af23861ba5c080e8dac0b26eb0
forfivo <forfivo@gmail.com>
2015-06-19 01:54:00 -0300

    [patch/adapted] add extra free kbytes tunable
    Add a userspace visible knob to tell the VM to keep an extra amount
    of memory free, by increasing the gap between each zone's min and
    low watermarks.
    
    This is useful for realtime applications that call system
    calls and have a bound on the number of allocations that happen
    in any short time period.  In this application, extra_free_kbytes
    would be left at an amount equal to or larger than than the
    maximum number of allocations that happen in any burst.
    
    It may also be useful to reduce the memory use of virtual
    machines (temporarily?), in a way that does not cause memory
    fragmentation like ballooning does.
    
    [ccross]
    Revived for use on old kernels where no other solution exists.
    The tunable will be removed on kernels that do better at avoiding
    direct reclaim.
    
    Change-Id: I765a42be8e964bfd3e2886d1ca85a29d60c3bb3e
    Signed-off-by: Rik van Riel<riel@redhat.com>
    Signed-off-by: Colin Cross <ccross@android.com>
    Git-commit: 92189d47f66c67e5fd92eafaa287e153197a454f
    Git-repo: https://android.googlesource.com/kernel/common/
    [bhargavuln@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Bhargav Upperla <bhargavuln@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -105,6 +105,7 @@ extern unsigned int core_pipe_limit;
 #endif
 extern int pid_max;
 extern int min_free_kbytes;
+extern int extra_free_kbytes;
 extern int wmark_min_kbytes, wmark_low_kbytes, wmark_high_kbytes;
 extern int min_free_order_shift;
 extern int pid_max_min, pid_max_max;
@@ -1387,6 +1388,14 @@ static struct ctl_table vm_table[] = {
 		.procname	= "min_free_kbytes",
 		.data		= &min_free_kbytes,
 		.maxlen		= sizeof(min_free_kbytes),
+		.mode		= 0644,
+		.proc_handler	= min_free_kbytes_sysctl_handler,
+		.extra1		= &zero,
+	},
+	{
+		.procname	= "extra_free_kbytes",
+		.data		= &extra_free_kbytes,
+		.maxlen		= sizeof(extra_free_kbytes),
 		.mode		= 0644,
 		.proc_handler	= min_free_kbytes_sysctl_handler,
 		.extra1		= &zero,
