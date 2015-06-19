b1856fcd54105e23edbcc9e2857380e51189aad6
forfivo <forfivo@gmail.com>
2015-06-19 03:11:51 -0300

    [patch/adapted] mm: cma: remove watermark hacks
    Commits 2139cbe627b8 ("cma: fix counting of isolated pages") and
    d95ea5d18e69 ("cma: fix watermark checking") introduced a reliable
    method of free page accounting when memory is being allocated from CMA
    regions, so the workaround introduced earlier by commit 49f223a9cd96
    ("mm: trigger page reclaim in alloc_contig_range() to stabilise
    watermarks") can be finally removed.
    
    Change-Id: Iae17de8185eeabffd46752dbaf819591e6585869
    CRs-Fixed: 480377
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-commit: bc357f431c836c6631751e3ef7dfe7882394ad67
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [lauraa@codeaurora.org: Context fixup in mmzone.h, keep zone definition in
    alloc_contig_range for other purposes]
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -63,10 +63,8 @@ enum {
 
 #ifdef CONFIG_CMA
 #  define is_migrate_cma(migratetype) unlikely((migratetype) == MIGRATE_CMA)
-#  define cma_wmark_pages(zone)	zone->min_cma_pages
 #else
 #  define is_migrate_cma(migratetype) false
-#  define cma_wmark_pages(zone) 0
 #endif
 
 #define for_each_migratetype_order(order, type) \
@@ -382,11 +380,6 @@ struct zone {
 	seqlock_t		span_seqlock;
 #endif
 #ifdef CONFIG_CMA
-	/*
-	 * CMA needs to increase watermark levels during the allocation
-	 * process to make sure that the system is not starved.
-	 */
-	unsigned long		min_cma_pages;
 	bool			cma_alloc;
 #endif
 	struct free_area	free_area[MAX_ORDER];
