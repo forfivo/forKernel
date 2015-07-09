ef5d1a997cbdc8ac6db2cbeeb975eb86c85dd28b
forfivo <forfivo@gmail.com>
2015-07-09 19:54:37 -0300

    [adapted] sched: Deliver sched_switch events to the current task
    Otherwise they can't be filtered for a defined task:
    
      perf record -e sched:sched_switch ./foo
    
    This command doesn't report any events without this patch.
    
    I think it isn't a security concern if someone knows who will
    be executed next - this can already be observed by polling /proc
    state. By default perf is disabled for non-root users in any case.
    
    I need these events for profiling sleep times.  sched_switch is used for
    getting callchains and sched_stat_* is used for getting time periods.
    These events are combined in user space, then it can be analyzed by
    perf tools.
    
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Arun Sharma <asharma@fb.com>
    Link: http://lkml.kernel.org/r/1342088069-1005148-1-git-send-email-avagin@openvz.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2964,14 +2964,14 @@ static inline void
 prepare_task_switch(struct rq *rq, struct task_struct *prev,
 		    struct task_struct *next)
 {
+#ifdef TRACE_CRAP
+	trace_sched_switch(prev, next);
+#endif
 	sched_info_switch(prev, next);
 	perf_event_task_sched_out(prev, next);
 	fire_sched_out_preempt_notifiers(prev, next);
 	prepare_lock_switch(rq, next);
 	prepare_arch_switch(next);
-#ifdef TRACE_CRAP
-	trace_sched_switch(prev, next);
-#endif
 }
 
 /**
