8d76242fe62b62f96f7a8fc444d377a9a798a4d8
forfivo <forfivo@gmail.com>
2015-07-08 23:33:27 -0300

    idr: deprecate idr_remove_all()
    There was only one legitimate use of idr_remove_all() and a lot more of
    incorrect uses (or lack of it).  Now that idr_destroy() implies
    idr_remove_all() and all the in-kernel users updated not to use it,
    there's no reason to keep it around.  Mark it deprecated so that we can
    later unexport it.
    
    idr_remove_all() is made an inline function calling __idr_remove_all()
    to avoid triggering deprecated warning on EXPORT_SYMBOL().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/idr.h
+++ include/linux/idr.h
@@ -82,7 +82,6 @@ int idr_for_each(struct idr *idp,
 void *idr_get_next(struct idr *idp, int *nextid);
 void *idr_replace(struct idr *idp, void *ptr, int id);
 void idr_remove(struct idr *idp, int id);
-void idr_remove_all(struct idr *idp);
 void idr_free(struct idr *idp, int id);
 void idr_destroy(struct idr *idp);
 void idr_init(struct idr *idp);
@@ -120,6 +119,8 @@ static inline void *idr_find(struct idr *idr, int id)
 	return idr_find_slowpath(idr, id);
 }
 
+void __idr_remove_all(struct idr *idp);
+
 /**
  * idr_get_new - allocate new idr entry
  * @idp: idr handle
@@ -131,6 +132,18 @@ static inline void *idr_find(struct idr *idr, int id)
 static inline int idr_get_new(struct idr *idp, void *ptr, int *id)
 {
 	return idr_get_new_above(idp, ptr, 0, id);
+}
+
+/**
+ * idr_remove_all - remove all ids from the given idr tree
+ * @idp: idr handle
+ *
+ * If you're trying to destroy @idp, calling idr_destroy() is enough.
+ * This is going away.  Don't use.
+ */
+static inline void __deprecated idr_remove_all(struct idr *idp)
+{
+	__idr_remove_all(idp);
 }
 
 /**
