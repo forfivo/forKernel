440d728369cd8de9ca9fabdf8b67ca7cfe5b7d82
forfivo <forfivo@gmail.com>
2015-07-09 01:54:16 -0300

    sched: Task placement for heterogeneous systems based on task load-tracking
    This patch introduces the basic SCHED_HMP infrastructure. Each class of
    cpus is represented by a hmp_domain and tasks will only be moved between
    these domains when their load profiles suggest it is beneficial.
    
    SCHED_HMP relies heavily on the task load-tracking introduced in Paul
    Turners fair group scheduling patch set:
    
    <https://lkml.org/lkml/2012/8/23/267>
    
    SCHED_HMP requires that the platform implements arch_get_hmp_domains()
    which should set up the platform specific list of hmp_domains. It is
    also assumed that the platform disables SD_LOAD_BALANCE for the
    appropriate sched_domains.
    Tasks placement takes place every time a task is to be inserted into
    a runqueue based on its load history. The task placement decision is
    based on load thresholds.
    
    There are no restrictions on the number of hmp_domains, however,
    multiple (>2) has not been tested and the up/down migration policy is
    rather simple.
    
    Signed-off-by: Morten Rasmussen <Morten.Rasmussen@arm.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1035,6 +1035,12 @@ static inline int test_sd_parent(struct sched_domain *sd, int flag)
 
 bool cpus_share_cache(int this_cpu, int that_cpu);
 
+#ifdef CONFIG_SCHED_HMP
+struct hmp_domain {
+	struct cpumask cpus;
+	struct list_head hmp_domains;
+};
+#endif /* CONFIG_SCHED_HMP */
 #else /* CONFIG_SMP */
 
 struct sched_domain_attr;
